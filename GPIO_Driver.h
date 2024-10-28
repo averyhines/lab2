97a389870b89ad1bb34d7dd1103cfeef6fe5c782
averyhines <avh428@lehigh.edu>
2024-10-28 16:02:11 -0400

    Add files via upload

---
+++ GPIO_Driver.h
@@ -0,0 +1,17 @@
+/*
+ * GPIO_Driver.h
+ *
+ *  Created on: Oct 28, 2024
+ *      Author: avh428
+ */
+
+#ifndef GPIO_DRIVER_H_
+#define GPIO_DRIVER_H_
+
+void gpioInitIn(unsigned int Port, unsigned char Pin);
+void gpioInitOut(unsigned int Port, unsigned char Pin);
+unsigned char gpioInitStatus(unsigned int Port, unsigned char Pin);
+void gpioWrite(unsigned int Port, unsigned char Pin, unsigned char value);
+
+
+#endif /* GPIO_DRIVER_H_ */
